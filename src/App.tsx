72e5ad6772046d49bc7ea41054c647d303059eff
hirohata <hirohatatro@gmail.com>
2025-03-14 17:55:47 -0700

    feat: added the geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,84 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
+import {
+	checkPermissions,
+	getCurrentPosition,
+	requestPermissions,
+	watchPosition,
+} from "@tauri-apps/plugin-geolocation";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+	const [greetMsg, setGreetMsg] = useState("");
+	const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		setGreetMsg(await invoke("greet", { name }));
+	}
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+	useEffect(() => {
+		async () => {
+			await getPermission();
+		};
+	}, []);
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+	async function getPermission() {
+		let permissions = await checkPermissions();
+		if (
+			permissions.location === "prompt" ||
+			permissions.location === "prompt-with-rationale"
+		) {
+			permissions = await requestPermissions(["location"]);
+		}
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+		if (permissions.location === "granted") {
+			const pos = await getCurrentPosition();
+
+			await watchPosition(
+				{ enableHighAccuracy: true, timeout: 10000, maximumAge: 0 },
+				(pos) => {
+					console.log(pos);
+				},
+			);
+		}
+	}
+
+	return (
+		<main className="container">
+			<h1>Welcome to Tauri + React</h1>
+
+			<div className="row">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src="/vite.svg" className="logo vite" alt="Vite logo" />
+				</a>
+				<a href="https://tauri.app" target="_blank" rel="noreferrer">
+					<img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+			<form
+				className="row"
+				onSubmit={(e) => {
+					e.preventDefault();
+					greet();
+				}}
+			>
+				<input
+					id="greet-input"
+					onChange={(e) => setName(e.currentTarget.value)}
+					placeholder="Enter a name..."
+				/>
+				<button type="submit">Greet</button>
+			</form>
+			<p>{greetMsg}</p>
+		</main>
+	);
 }
 
 export default App;
