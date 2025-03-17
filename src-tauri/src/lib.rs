74abc720fce163dc72ecdff2384b580ba305749c
hirohata <hirohatatro@gmail.com>
2025-03-17 15:24:58 -0700

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,17 @@
+use tauri::Emitter;
+use tauri_plugin_geolocation::GeolocationExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[derive(Clone, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Position {
+    latitude: f64,
+    longitude: f64,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,6 +19,28 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_geolocation::init())
+        .setup(|app| {
+            let location = std::sync::Arc::new(app.handle().clone());
+            let loc = location.clone();
+            println!("setup started");
+            std::thread::spawn(move || loop {
+                let location = loc
+                    .geolocation()
+                    .get_current_position(None)
+                    .unwrap_or_else(|e| panic!("{:?}", e));
+                println!("{:?}, {:?}", location.coords.latitude, location.coords.longitude);
+                loc.emit(
+                    "location",
+                    Position {
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                    },
+                )
+                .unwrap_or_else(|e| panic!("{:?}", e));
+                std::thread::sleep(std::time::Duration::from_millis(1000));
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import {
 	checkPermissions,
-	getCurrentPosition,
 	requestPermissions,
 	watchPosition,
 } from "@tauri-apps/plugin-geolocation";
+import {listen} from "@tauri-apps/api/event";
+
+type LocationType = {
+	longitude: number;
+	latitude: number;
+}
 
 function App() {
 	const [greetMsg, setGreetMsg] = useState("");
 	const [name, setName] = useState("");
+	const [location, setLocation] = useState<LocationType>({longitude: 0, latitude: 0})
+	const [permission, setPermission] = useState<boolean>(false);
 
 	async function greet() {
 		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -19,23 +26,37 @@ function App() {
 	}
 
 	useEffect(() => {
-		async () => {
+		console.log("effect");
+		(async () => {
 			await getPermission();
+		})();
+	}, []);
+
+	useEffect(() => {
+		const unlisten = listen<{latitude: number, longitude: number}>('location', (event) => {
+			console.log(event.payload);
+			setLocation({latitude: event.payload.latitude, longitude: event.payload.longitude})
+    	});
+
+		return () => {
+		  unlisten.then(f => f());
 		};
 	}, []);
 
 	async function getPermission() {
 		let permissions = await checkPermissions();
+		console.log(permissions);
 		if (
 			permissions.location === "prompt" ||
 			permissions.location === "prompt-with-rationale"
 		) {
+			console.log("location", permissions.location);
 			permissions = await requestPermissions(["location"]);
+			console.log("location", permissions.location);
+			setPermission(permissions.location === "granted");
 		}
 
 		if (permissions.location === "granted") {
-			const pos = await getCurrentPosition();
-
 			await watchPosition(
 				{ enableHighAccuracy: true, timeout: 10000, maximumAge: 0 },
 				(pos) => {
@@ -75,8 +96,12 @@ function App() {
 					placeholder="Enter a name..."
 				/>
 				<button type="submit">Greet</button>
+				{/*<button type="button" onClick={getPermission}>Greet</button>*/}
 			</form>
 			<p>{greetMsg}</p>
+			<p>Permission: {permission ? "granted" : "denied"}</p>
+			<p>Longitude: {location.longitude}</p>
+			<p>Latitude: {location.latitude}</p>
 		</main>
 	);
 }
